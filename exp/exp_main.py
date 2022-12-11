eb9b4ffd4010066c172cdf3b4420a41eec92a176
Wiqzard <s.stapf@aol.com>
2022-12-11 07:40:21 +0100

    fix

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -4,7 +4,7 @@ from portfolio_manager.algorithms import *
 from environment.environment import Environment
 from agent.agent import Agent
 from utils.constants import *
-from exp_basic import Exp_Basic
+from exp.exp_basic import Exp_Basic
 
 
 class Exp_Main(Exp_Basic):

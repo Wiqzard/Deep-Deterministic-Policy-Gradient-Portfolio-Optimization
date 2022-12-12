7202ba48246e23d4a2942a63900eb92c0bdb250d
Wiqzard <s.stapf@aol.com>
2022-12-12 17:08:52 +0100

    fix

--- exp/exp_fed.py
+++ exp/exp_fed.py
@@ -272,7 +272,7 @@ class Exp_Fed(Exp_Basic):
                 # mu0 = mu0 if isinstance(mu0, float) else mu0[:, None]
                 factor2 = (
                     1
-                    - c * w_t[:, 0]
+                    - c * w_t[0]
                     - (2 * c - c**2)
                     * torch.nn.functional.relu(w_t[1:] - mu0 * w_t_1[1:]).sum(axis=1)
                 )

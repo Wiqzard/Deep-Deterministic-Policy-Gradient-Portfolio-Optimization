9b3efaad35d4ea50b7ae2477bb5e397da7999732
Wiqzard <s.stapf@aol.com>
2022-12-12 20:22:52 +0100

    fix

--- exp/exp_fed.py
+++ exp/exp_fed.py
@@ -279,7 +279,8 @@ class Exp_Fed(Exp_Basic):
             mu = recurse(mu)
         print(y_t.shape)
         print(w_t_1.shape)
-        r_t = torch.log(mu * torch.sum(y_t * w_t_1, dim=0, keepdim=True))  # .squeeze()
+        print(mu * torch.sum(y_t * w_t_1, dim=1, keepdim=True))
+        r_t = torch.log(mu * torch.sum(y_t * w_t_1, dim=1, keepdim=True))  # .squeeze()
         rewards = r_t.tolist()
         print(rewards)
         return rewards[-1]
